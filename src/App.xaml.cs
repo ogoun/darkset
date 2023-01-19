28620403134ce4db85c3a2a5bcf0b95e6bb9777d
Ogoun <ogoun@users.noreply.github.com>
2023-01-19 21:08:06 +0300

    Update

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics;
-using System.IO;
-using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using ZeroLevel;
 
 namespace Darknet.Dataset.Merger
@@ -15,36 +12,6 @@ namespace Darknet.Dataset.Merger
         {
             Log.AddTextFileLogger(new ZeroLevel.Logging.TextFileLoggerOptions().SetFolderPath("logs"), ZeroLevel.Logging.LogLevel.FullStandart);
             base.OnStartup(e);
-            //CheckForUpdatesService();
         }
-        /*
-        private static void CheckForUpdatesService()
-        {
-            ThreadPool.QueueUserWorkItem((s) =>
-            {
-                var folder = Configuration.BaseDirectory;
-                try
-                {
-                    var cp_path = Path.Combine(folder, "update.exe");
-                    if (false == File.Exists(cp_path))
-                    {
-                        File.WriteAllBytes(cp_path, Darknet.Dataset.Merger.Properties.Resources.Installer);
-                    }
-                    var update_service = new Process();
-                    update_service.StartInfo = new ProcessStartInfo
-                    {
-                        CreateNoWindow = true,
-                        FileName = cp_path,
-                        UseShellExecute = true,
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    };
-                    update_service.Start();
-                    update_service.WaitForExit();
-                }
-                catch
-                {
-                }
-            });
-        }*/
     }
 }
